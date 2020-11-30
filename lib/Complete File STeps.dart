4550f59920103db1c9ae9682db91fa6cea0912a6
abdullahwale <abdullahwale@gmail.com>
2020-11-30 10:53:58 +0500

    Step 02 Seprate the Code of main Exsisting code Create "widget/mapFile" Folder Add map Flutter Project

--- lib/Complete File STeps.dart
+++ lib/Complete File STeps.dart
@@ -6,3 +6,47 @@
 //Create Flutter Project
 //Integrate API key
 //Display Google Maps
+//TODO Step 02 Seprate the Code of main Exsisting code Create "widget/mapFile" Folder
+//TODO Step 03 Add map Flutter Project
+/*
+or
+google_maps_flutter: ^1.0.6
+https://pub.dev/packages/google_maps_flutter
+////////
+google_maps_flutter:
+    git:
+      url: git://github.com/flutter/plugins
+      path: packages/google_maps_flutter
+
+      import 'package:google_maps_flutter/google_maps_flutter.dart';
+ */
+//TODO Add GoogleMap Properties
+/*
+class _MyHomePageState extends State<MyHomePage> {
+  GoogleMapController mapController;
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Map"),
+      ),
+      body: Center(
+        child: SizedBox(
+          child: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: LatLng(51.5160895, -0.1294527),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+ */
